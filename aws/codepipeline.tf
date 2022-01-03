0b3a144bfc8e5a066565f9f6c291221c184fbdff
Chester ONeill <coneillcodes@gmail.com>
2022-01-02 21:03:21 -0500

    Fix codepipeline

--- aws/codepipeline.tf
+++ aws/codepipeline.tf
@@ -3,7 +3,7 @@ resource "aws_codepipeline" "codepipeline" {
   role_arn = aws_iam_role.codepipeline_role.arn
 
   artifact_store {
-    location = aws_s3_bucket.codepipeline_bucket.bucket
+    location = data.aws_s3_bucket.codepipeline_bucket.bucket
     type     = "S3"
   }
 
@@ -38,8 +38,9 @@ resource "aws_codepipeline" "codepipeline" {
       output_artifacts = ["build_output"]
       version          = "1"
 
+
       configuration = {
-        ProjectName = "test"
+        ProjectName = "test-project"
       }
     }
   }
@@ -48,8 +49,6 @@ resource "aws_codepipeline" "codepipeline" {
 data "aws_codestarconnections_connection" "github" {
   arn = "arn:aws:codestar-connections:us-east-1:063754174791:connection/f82bad92-5580-4ef2-8ae7-21688ba9c04f"
 }
-
-
 
 resource "aws_iam_role" "codepipeline_role" {
   name = "test-role"
@@ -62,6 +61,13 @@ resource "aws_iam_role" "codepipeline_role" {
       "Effect": "Allow",
       "Principal": {
         "Service": "codepipeline.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    },
+     {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codebuild.amazonaws.com"
       },
       "Action": "sts:AssumeRole"
     }
@@ -81,16 +87,15 @@ resource "aws_iam_role_policy" "codepipeline_policy" {
         {
           "Effect" : "Allow",
           "Action" : [
-            "s3:*",
-            # "s3:GetObject",
-            # "s3:GetObjectVersion",
-            # "s3:GetBucketVersioning",
-            # "s3:PutObjectAcl",
-            # "s3:PutObject"
+            "s3:GetObject",
+            "s3:GetObjectVersion",
+            "s3:GetBucketVersioning",
+            "s3:PutObjectAcl",
+            "s3:PutObject"
           ],
           "Resource" : [
-            "${aws_s3_bucket.codepipeline_bucket.arn}",
-            "${aws_s3_bucket.codepipeline_bucket.arn}/*",
+            "${data.aws_s3_bucket.codepipeline_bucket.arn}",
+            "${data.aws_s3_bucket.codepipeline_bucket.arn}/*",
             "${aws_s3_bucket.website.arn}/*",
             "${aws_s3_bucket.website.arn}",
           ]
@@ -109,16 +114,46 @@ resource "aws_iam_role_policy" "codepipeline_policy" {
             "codebuild:StartBuild"
           ],
           "Resource" : "*"
+        },
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
+          ]
+          "Resource" : "*"
         }
       ]
   })
 }
 
-# data "aws_s3_bucket" "codepipeline" {
-#   bucket = "codepipeline-us-east-1-463096050773"
-# }
+resource "aws_codebuild_project" "example" {
+  name          = "test-project"
+  description   = "test_codebuild_project"
+  build_timeout = "5"
+  service_role  = aws_iam_role.codepipeline_role.arn
 
-resource "aws_s3_bucket" "codepipeline_bucket" {
-  bucket = "cjo-codepipeline"
-  acl    = "private"
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:5.0"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+  }
+  source {
+    type = "CODEPIPELINE"
+  }
 }
+
+data "aws_s3_bucket" "codepipeline_bucket" {
+  bucket = "codepipeline-us-east-1-463096050773"
+}
+
+# resource "aws_s3_bucket" "codepipeline_bucket" {
+#   bucket = "cjo-codepipeline"
+#   acl    = "private"
+# }
